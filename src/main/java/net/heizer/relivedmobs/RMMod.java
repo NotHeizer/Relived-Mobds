300fa74a1bd3b9cde2ea8952f3743f02e0af637d
NotHeizer <hafner.joel@icloud.com>
2022-05-02 17:32:45 +0200

    Add Mod Workspace to GitHub / First Commit

---
+++ src/main/java/net/heizer/relivedmobs/RMMod.java
@@ -0,0 +1,34 @@
+package net.heizer.relivedmobs;
+
+import com.mojang.logging.LogUtils;
+import net.minecraft.world.level.block.Blocks;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import org.slf4j.Logger;
+
+
+@Mod(RMMod.MOD_ID)
+public class RMMod
+{
+    public static final String MOD_ID = "relivedmobs";
+
+    // Directly reference a slf4j logger
+    private static final Logger LOGGER = LogUtils.getLogger();
+
+    public RMMod()
+    {
+        // Register the setup method for modloading
+        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
+
+        // Register ourselves for server and other game events we are interested in
+        MinecraftForge.EVENT_BUS.register(this);
+    }
+
+    private void setup(final FMLCommonSetupEvent event) {
+        // some preinit code
+        LOGGER.info("HELLO FROM PREINIT");
+        LOGGER.info("DIRT BLOCK >> {}", Blocks.DIRT.getRegistryName());
+    }
+}
